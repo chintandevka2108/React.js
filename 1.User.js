7447c9794bfc9306e9991b6d8cfeafd418879e6f
chintandevka2108 <168894945+chintandevka2108@users.noreply.github.com>
2025-02-11 03:50:30 -0800

    Add files via upload

---
+++ 1.User.js
@@ -0,0 +1,8 @@
+const Yash = {
+    Name : "Yash",
+    Age : 65,
+    Country : "Russia"
+}
+const {Name,Age,Country} = Yash;
+
+console.log(Name,Age,Country);
---
+++ 2.Colors.js
@@ -0,0 +1,5 @@
+const colors = ["red", "blue",  "yellow"];
+
+const  [a, b ,c] = colors;
+
+console.log(a, b ,c);
